7aeb6ff7a918f62ad9dd3ff74577de44a324e661
Andy Fan <andyvan129@gmail.com>
2024-08-06 12:18:49 -0500

    old

--- Machine Learning Section 2.R
+++ Machine Learning Section 2.R
@@ -1,3 +1,19 @@
 mnist <- read_mnist()
 head(mnist)
 ncol(mnist$train$images)
+
+
+# 2.1 Q2
+if (!require("dplyr", "lubridate")) install.packages("dplyr", "lubridate")
+library(dslabs)
+library(dplyr)
+library(lubridate)
+data(reported_heights)
+
+dat <- mutate(reported_heights, date_time = ymd_hms(time_stamp)) %>%
+  filter(date_time >= make_date(2016, 01, 25) & date_time < make_date(2016, 02, 1)) %>%
+  mutate(type = ifelse(day(date_time) == 25 & hour(date_time) == 8 & between(minute(date_time), 15, 30), "inclass","online")) %>%
+  select(sex, type)
+
+y <- factor(dat$sex, c("Female", "Male"))
+x <- dat$type
