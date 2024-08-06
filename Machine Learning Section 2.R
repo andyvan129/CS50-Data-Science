575949fdf640301335f68e1519d57eddfe8e719d
Andy Fan <andyvan129@gmail.com>
2024-08-06 05:32:21 +0000

    2.1 Q2

--- Machine Learning Section 2.R
+++ Machine Learning Section 2.R
@@ -1,3 +1,26 @@
 mnist <- read_mnist()
 head(mnist)
 ncol(mnist$train$images)
+
+packages <- c("dplyr", "lubridate", "dslabs")
+for (package in packages) {
+  if (!require(package, character.only = TRUE)) install.packages(package)
+  library(package, character.only = TRUE)
+}
+
+data(reported_heights)
+
+dat <- mutate(reported_heights, date_time = ymd_hms(time_stamp)) %>%
+  filter(date_time >= make_date(2016, 01, 25) & date_time < make_date(2016, 02, 1)) %>%
+  mutate(type = ifelse(day(date_time) == 25 & hour(date_time) == 8 & between(minute(date_time), 15, 30), "inclass","online")) %>%
+  select(sex, type)
+
+y <- factor(dat$sex, c("Female", "Male"))
+x <- dat$type
+
+dat %>%
+  group_by(type) %>%
+  summarise(female = mean(sex == 'Female'), male = mean(sex == 'Male'))
+
+y_hat <- ifelse(x == 'inclass', 'Female', 'Male')
+mean(y == y_hat)
