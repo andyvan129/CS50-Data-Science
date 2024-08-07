6a5fdea2d89f53548d62aa146cc5090617ed5658
Andy Fan <andyvan129@gmail.com>
2024-08-07 13:46:34 +0000

    Section 3 Q1

---
+++ Machine Learning Section 3.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(caret)
+
+set.seed(1)
+n <- 100
+Sigma <- 9*matrix(c(1.0, 0.5, 0.5, 1.0), 2, 2)
+dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>%
+  data.frame() %>% setNames(c("x", "y"))
